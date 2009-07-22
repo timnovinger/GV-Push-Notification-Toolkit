870cea78c1e7e14c707d60f2ecf853a97c249fb7
Tim Novinger <tim.novinger@gmail.com>
2009-07-21 23:21:33 -0400

    split scripts and enhanced overall documentation

---
+++ AJAX-Push-Notification-Quick-Refresh.user.js
@@ -0,0 +1,110 @@
+// ==UserScript==
+// @name        AJAX Push Notification Quick Refresh
+// @namespace   http://fluidapp.com
+// @description Uses XHR requests on a timer to speed up the refresh rate of the GV SMS Inbox for pushing out to Prowl for enhanced iPhone OS Push Notifications
+// @include     https://www.google.com/voice*
+// @include     http://www.google.com/voice*
+// @author      Tim Novinger {http://www.github.com/timnovinger} and Mike Krisher {http://www.github.com/mkrisher}
+// ==/UserScript==
+
+(function () {
+    if (window.fluid) 
+    {
+		
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		// Setup variables
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		var xmlhttp;
+		var currentCount = 0;
+		var url = 'https://www.google.com/voice/inbox/recent/';
+		
+		
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//! INITIALIZE
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		var init = setInterval(Ajax, 5000);
+		
+		
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//!  AJAX REQUEST
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		function Ajax()
+		{
+			xmlhttp = null;
+			xmlhttp = new XMLHttpRequest();
+		
+			if (xmlhttp != null) {
+				xmlhttp.onreadystatechange = state_Change;
+				xmlhttp.open("GET",url,true);
+				xmlhttp.send(null);
+			} else {
+				alert("Your browser does not support XMLHTTP.");
+			}
+		}
+		
+		
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//!  STATE CHANGE HANDLER
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////	
+		function state_Change()
+		{
+			if (xmlhttp.readyState == 4) { 								// 4 = "loaded"
+				if (xmlhttp.status == 200) {							// 200 = OK
+					
+					//save ajax response
+					var json = xmlhttp.responseText;
+					
+					//parse response string as XML
+					var xmlobject = (new DOMParser()).parseFromString(json, "text/xml");
+					
+					//look for only JSON data within XML
+					var messages = xmlobject.getElementsByTagName('json')[0];
+					
+					//eval it and turn it into a JSON object
+					var msgObj = eval( '(' + messages.firstChild.nodeValue + ')' );
+					
+					//parse JSON object for SMS notification
+					currentCount = getByAjaxSMS(msgObj);
+					
+					//console.log(currentCount);
+				} else {
+					alert("Problem retrieving XML data");
+				}
+			}
+		}
+		
+		
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//!  GET SMS via AJAX
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		function getByAjaxSMS(msgObj){
+			//get unread SMS count
+			var count = Number(msgObj['unreadCounts']['sms']);
+			
+			//determine message gramar
+			if(count == 1){
+				var postfix = '';
+			} else {
+				var postfix = 's';
+			}
+		   	
+		   	//format message
+		   	var msg = count+' new SMS message'+postfix;
+		
+			//only push out if we've received a NEW message
+			if(count>currentCount)
+			{	
+				//send to Prowl
+				fluid.showGrowlNotification({
+					title: "Google Voice",
+					description: msg,
+					priority: 3,
+						sticky: false
+				});
+			}
+			//console.log(count +" " + currentCount);
+			return count;
+		}
+		
+    }
+})();
--- README.md
+++ README.md
@@ -1,28 +1,29 @@
 #Google Voice Toolkit
 
 ##INSTALL GROWL
-- Included
+- Using the included installer
 
 ##INSTALL FLUID.APP
-- Included
+- Using the included installer
 
 ##INSTALL PROWL
 - On your iPhone/iPod Touch    http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=320876271&mt=8 
 - On your Computer using the included Prowl.growlView 
-	- This can also be a SERVER if you have one so that these notifications are always available.
+	- This can also be installed on a SERVER if you have one. In this setup, notifications are always available to be pushed to you.
 
 ##CREATE GOOGLE VOICE APPLICATION
-- Name application Google Voice
-- Use Google-Voice-256.png as the icon
-- URL is http://www.google.com/voice
-- Make SURE that allowed browsing URLs are ONLY the following or it will try to open links up inside Safari instead (asterisks ARE necessary)
+- Name the new Fluid application "Google Voice"
+- Use Google-Voice-256.png as the icon for it
+- Set the URL as: http://www.google.com/voice
+- Make SURE that the allowed browsing URLs are ONLY the following, or it will attempt to open links inside of your default browser instead of within your new Fluid application. (asterisks ARE necessary)
 	- *google.com/voice*
 	- *google.com*/accounts/ServiceLogin*
 
 ##SETUP SMS GROWL NOTIFICATIONS
-- copy 50521.user.js (don't change the filename) in ~/Library/Application Support/Fluid/SSB/Google Voice/Userscripts
+- Copy GV-Growl-Notifications.user.js to 			~/Library/Application Support/Fluid/SSB/Google Voice/Userscripts
+- Copy AJAX-Push-Notification-Quick-Refresh.user 	~/Library/Application Support/Fluid/SSB/Google Voice/Userscripts
 
 ##SETUP PROWL NOTIFICATIONS
-- Go into the Google Voice application in the applications list
+- Go into the Google Voice application in the Growl applications list
 - Select Prowl for your Javascript Notification
 - Make sure the Prowl plugin for Growl is setup with their Push Notification server (register, enter your credentials in, send a test message)
